5fcbc7bf166f11c689de82d6e1cdb903e19bf005
Ragu Balan <155983587+ragubalankumar@users.noreply.github.com>
2026-04-20 19:13:41 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,43 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (header) {
+    window.addEventListener("scroll", () => {
+      header.classList.toggle("sticky", window.scrollY > 0);
+    });
+  }
+});
+
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalcloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+  if (serviceModals[modalClick]) {
+    serviceModals[modalClick].classList.add("active");
+  }
+};
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+  learnmoreBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalcloseBtns.forEach((modalCloseBtn) => {
+  modalCloseBtn.addEventListener("click", () => {
+    serviceModals.forEach((modalview) => {
+      modalview.classList.remove("active");
+    });
+  });
+});
+
+const scrollTopBtn = document.querySelector(".scrollTotop-btn");
+
+window.addEventListener("scroll", function () {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
